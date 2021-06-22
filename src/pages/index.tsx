b240dda84479d4c5752a6ea836ccf7da540b97fe
edmilson-dk <droidvidaboa@gmail.com>
2021-06-21 23:08:14 -0300

    feat: added button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,31 +2,34 @@ import illustrationSvg from "../assets/images/illustration.svg";
 import logoSvg from "../assets/images/logo.svg";
 import googleSvg from "../assets/images/google-icon.svg";
 
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
+
 export function Home() {
   return (
-    <section>
+    <section id="page-auth">
       <aside>
         <img src={illustrationSvg} alt="Illustration" loading="lazy"/>
         <strong>Crie sala de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas audiência em tempo-real.</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoSvg} alt="Letmeask" />
-          <button>
+          <button className="create-room">
             <img src={googleSvg} alt="Google icon" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
 
           <form>
             <input 
               type="text"
               placeholder="Digite o código da sala"
             />
-            <button type="button">
+            <Button type="submit">
               Entrar na sala
-            </button>
+            </Button>
           </form>
         </div>
       </main>
