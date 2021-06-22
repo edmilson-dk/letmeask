3c58a0a5c269fb2ffb0e8b637b3f8ef01144b96f
edmilson-dk <droidvidaboa@gmail.com>
2021-06-22 10:18:53 -0300

    feat: added useHistory

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import {  useHistory } from "react-router-dom";
+
 import illustrationSvg from "../../assets/images/illustration.svg";
 import logoSvg from "../../assets/images/logo.svg";
 import googleSvg from "../../assets/images/google-icon.svg";
@@ -7,6 +9,12 @@ import "../../styles/auth.scss";
 import { Button } from "../../components/Button";
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+    history.push("/rooms/new");
+  }
+
   return (
     <section id="page-auth">
       <aside>
@@ -17,7 +25,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoSvg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={navigateToNewRoom} className="create-room">
             <img src={googleSvg} alt="Google icon" />
             Crie sua sala com o Google
           </button>
