c4f1d84ccf6172bc6552577afd21959da0dac2e6
edmilson-dk <droidvidaboa@gmail.com>
2021-06-22 20:04:40 -0300

    feat: added auth-context-provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/rooms/new" component={NewRoom} />
+        <AuthContextProvider>
+          <Route exact path="/" component={Home}/>
+          <Route path="/rooms/new" component={NewRoom} />
+        </AuthContextProvider>
       </Switch>
     </BrowserRouter>
   );
