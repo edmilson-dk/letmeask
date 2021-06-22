a89a10b56f3145785dddb3f6628360b19fa9e78a
edmilson-dk <droidvidaboa@gmail.com>
2021-06-21 21:30:39 -0300

    feat: added global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './services/firebase';
-
-import App from './App';
+import "./services/firebase";
+import "./styles/global.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
