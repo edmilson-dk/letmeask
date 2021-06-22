0a5c8607370e3ca368008af9a8ee0fcf24991b32
edmilson-dk <droidvidaboa@gmail.com>
2021-06-22 10:19:25 -0300

    feat: created routes menager structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-   <NewRoom />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/rooms/new" component={NewRoom} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
