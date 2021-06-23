3b3480f07b76934fbb0bc4cbf03cc07f3b47dcfa
edmilson-dk <droidvidaboa@gmail.com>
2021-06-22 22:15:40 -0300

    feat: added new router

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <AuthContextProvider>
-          <Route exact path="/" component={Home}/>
-          <Route path="/rooms/new" component={NewRoom} />
-        </AuthContextProvider>
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
