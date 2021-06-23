8714589d430f52f59296d33c94e163d4b65ce79a
edmilson-dk <droidvidaboa@gmail.com>
2021-06-23 10:44:25 -0300

    feat: created room page with initial structure

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,5 +1,38 @@
+import logoSvg from "../../assets/images/logo.svg";
+
+import { Button } from "../../components/Button";
+
+import "../../styles/room.scss";
+
 export function Room() {
   return (
-    <h1>Room page</h1>
+    <div id="page-room">
+      <header>
+        <div className="header-content">
+          <img src={logoSvg} alt="Logo" />
+          <div>Código</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala react</h1>
+          <span>
+            4 perguntas
+          </span>
+        </div>
+
+        <form>
+          <textarea 
+            placeholder="O que você quer perguntar?"
+          />
+
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button type="button">faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
   )
 }
