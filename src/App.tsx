d669c3423716e1f7e0d1872aa1bbf44f0becccfb
edmilson-dk <droidvidaboa@gmail.com>
2021-06-22 23:26:14 -0300

    other: added react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContext";
@@ -8,6 +11,7 @@ import { Room } from "./pages/Room";
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <AuthContextProvider>
         <Switch>
           <Route exact path="/" component={Home} />
