913d34580370bfd9344b413a75caba3f64d9169c
edmilson-dk <droidvidaboa@gmail.com>
2021-06-26 13:15:30 -0300

    feat: added validate if room alredy closed

--- src/hooks/useRoom/index.ts
+++ src/hooks/useRoom/index.ts
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
 
 import { database } from "../../services/firebase";
 import { parseFirebaseQuestions } from "../../utils/parseFirebaseQuestions";
@@ -8,6 +10,7 @@ import { useAuthContext } from "../useAuthContext";
 
 export function useRoom(roomId: string): UseRoomReturnTypes {
   const { user } = useAuthContext();
+  const history = useHistory();
   const [roomTitle, setRoomTitle] = useState("");
   const [questions, setQuestions] = useState<QuestionsType[]>([]);
 
@@ -17,7 +20,16 @@ export function useRoom(roomId: string): UseRoomReturnTypes {
     roomRef.on("value", room =>{
       const databaseRoom = room.val();
       const firebaseQuestions: FirebaseQuestionsType = databaseRoom.questions ?? {};
-     
+      
+      if (databaseRoom.endedAt){
+        if (databaseRoom.authorId !== user?.id) {
+          toast.warning("This room has been closed by the administrator");
+        }
+        
+        history.push("/");
+        return;
+      }
+
       const parsedQuestions = parseFirebaseQuestions(firebaseQuestions, user);
   
       setRoomTitle(databaseRoom.title);
@@ -25,7 +37,7 @@ export function useRoom(roomId: string): UseRoomReturnTypes {
     });
 
     return () => roomRef.off("value");
-  }, [roomId, user]);
+  }, [roomId, user, history]);
 
   return {
     questions,
