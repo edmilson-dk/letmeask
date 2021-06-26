def7ad37b945da4421056aa1f4e56469a42ec16d
edmilson-dk <droidvidaboa@gmail.com>
2021-06-26 11:55:36 -0300

    feat: added new parsed questions return property value

--- src/utils/parseFirebaseQuestions.ts
+++ src/utils/parseFirebaseQuestions.ts
@@ -1,14 +1,23 @@
-import { FirebaseQuestionsType } from "../types/pages/Room";
+import { UserType } from "../types/contexts/AuthContext";
+import { FirebaseQuestionsType } from "../types/hooks/useRoom";
 
-export function parseFirebaseQuestions(firebaseQuestions: FirebaseQuestionsType) {
-  const parsedQuestions = Object.entries(firebaseQuestions)
-    .map(([key, value]) => {
+export function parseFirebaseQuestions(
+  firebaseQuestions: FirebaseQuestionsType, 
+  user: UserType | undefined,
+  isChildAdded = false,
+) {
+  const receivedData = Object.entries(firebaseQuestions);
+  const data = isChildAdded ? [receivedData[receivedData.length-1]] : receivedData;
+ 
+  const parsedQuestions = data.map(([key, value]) => {
       return {
         id: key,
         content: value.content,
         author: value.author,
         isHighLighted: value.isHighLighted,
-        isAnswered: value.isAnswered
+        isAnswered: value.isAnswered,
+        likesCount: Object.values(value.likes ?? {}).length,
+        likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
       }
     });
 
