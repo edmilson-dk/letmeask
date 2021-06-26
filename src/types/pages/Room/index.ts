067fd10c5e905c1b46cc5b69f0bd318de51d347e
edmilson-dk <droidvidaboa@gmail.com>
2021-06-26 12:05:02 -0300

    feat: added new room page types

--- src/types/pages/Room/index.ts
+++ src/types/pages/Room/index.ts
@@ -1,24 +1,3 @@
-export type FirebaseQuestionsType = Record<string, {
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isHighLighted: string;
-  isAnswered: string;
-}>
-
-export type QuestionsType = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  };
-  content: string;
-  isHighLighted: string;
-  isAnswered: string;
-}
-
 export type RoomParamsType = {
   id: string;
 }
