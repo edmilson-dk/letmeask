87e73cadc5c3d099abd9756d7f9492089bf90144
edmilson-dk <droidvidaboa@gmail.com>
2021-06-26 12:01:54 -0300

    feat: finished question handle like

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -22,8 +22,8 @@ export function Room() {
   const { user } = useAuthContext();
   const roomId = params.id;
 
-  const [ newQuestion, setNewQuestion ] = useState("");
-  const { questions, setIsSendNewQuestion, roomTitle } = useRoom(roomId);
+  const [newQuestion, setNewQuestion] = useState("");
+  const { questions, roomTitle } = useRoom(roomId);
 
   async function handleSendQuestion(e: FormEvent) {
     e.preventDefault();
@@ -50,10 +50,25 @@ export function Room() {
     try {
       await database.ref(`rooms/${roomId}/questions`).push(question);
 
-      setIsSendNewQuestion(true);
       toast.success("Your question has been sent successfully!");
       setNewQuestion("");
-    } catch(err) {
+    } catch (err) {
+      toast.warning("An unexpected error has occurred, please try again.");
+    }
+  }
+
+  async function handleLikeQuestion(questionId: string, likeId: string | undefined) {
+    try {
+      if (!likeId) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}/likes`)
+          .push({ authorId: user?.id });
+
+        toast.success("Like successfully added!");
+      } else {
+        await database.ref(`rooms/${roomId}/questions/${questionId}/likes/${likeId}`).remove();
+        toast.success("Like successfully removed!");
+      }
+    } catch (err) {
       toast.warning("An unexpected error has occurred, please try again.");
     }
   }
@@ -63,28 +78,28 @@ export function Room() {
       <header>
         <div className="header-content">
           <img src={logoSvg} alt="Logo" />
-          <RoomCode code={roomId}/>
+          <RoomCode code={roomId} />
         </div>
       </header>
 
       <main className="content">
         <div className="room-title">
           <h1>Sala {roomTitle}</h1>
-          { questions.length > 0 && <span>{ questions.length } perguntas</span>}
+          {questions.length > 0 && <span>{questions.length} perguntas</span>}
         </div>
 
         <form onSubmit={handleSendQuestion}>
-          <textarea 
+          <textarea
             placeholder="O que você quer perguntar?"
             onChange={(e) => setNewQuestion(e.target.value)}
             value={newQuestion}
           />
 
           <div className="form-footer">
-            { user ? (
+            {user ? (
               <div className="user-info">
                 <img src={user.avatar} alt={user.name} />
-                <span>{ user.name }</span>
+                <span>{user.name}</span>
               </div>
             ) : (
               <span>Para enviar uma pergunta, <button type="button">faça seu login</button>.</span>
@@ -95,12 +110,27 @@ export function Room() {
 
         <article className="questions-list">
           {
-            questions.length > 0 && questions.map(({ author, id, content }) => (
-              <Question 
-                key={id} 
-                author={author} 
+            questions.length > 0 && questions.map(({ author, id, content, likesCount, likeId }) => (
+              <Question
+                key={id}
+                author={author}
                 content={content}
-              />
+              >
+                <button
+                  className={`question-like-btn ${likeId && 'liked'}`}
+                  type="button"
+                  aria-label="Marcar como gostei"
+                  onClick={() => handleLikeQuestion(id, likeId)}
+                >
+                  {likesCount > 0 && <span>{likesCount}</span>}
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M7 22H4C3.46957 22 2.96086 21.7893 2.58579 21.4142C2.21071 21.0391 2 20.5304 2 20V13C2 12.4696 2.21071 11.9609 2.58579 11.5858C2.96086 11.2107 3.46957 11 4 11H7M14 9V5C14 4.20435 13.6839 3.44129 13.1213 2.87868C12.5587 2.31607 11.7956 2 11 2L7 11V22H18.28C18.7623 22.0055 19.2304 21.8364 19.5979 21.524C19.9654 21.2116 20.2077 20.7769 20.28 20.3L21.66 11.3C21.7035 11.0134 21.6842 10.7207 21.6033 10.4423C21.5225 10.1638 21.3821 9.90629 21.1919 9.68751C21.0016 9.46873 20.7661 9.29393 20.5016 9.17522C20.2371 9.0565 19.9499 8.99672 19.66 9H14Z"
+                      stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+              </Question>
             ))
           }
         </article>
